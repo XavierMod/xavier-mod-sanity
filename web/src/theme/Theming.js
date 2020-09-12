0747000fb854e1c5b4d19452e6e33130e66adbac
Xavier Mod <hi.xavier.mod@gmail.com>
2020-09-12 23:36:21 +0200

    new attempt - babel plugin styled components added

--- web/src/theme/Theming.js
+++ web/src/theme/Theming.js
@@ -1,25 +1,11 @@
 import React from 'react'
 import {ThemeProvider} from 'styled-components'
+import {theme} from './theme'
 import GlobalStyle from './globalStyles'
 
 const Theming = (props) => {
     return (
-        <ThemeProvider theme={{
-            colors: {
-                mainBackground: '#14161A',
-                darkBackground: '#131416',
-                textPrimary: '#ffffff',
-                textDark: '#14161A',
-                accent: '#ffffff'
-            },
-            fonts: {
-                primary: 'Bitter'
-            },
-            options: {
-                maxWidthLayout: '1200px',
-                logoWidth: '28px'
-            }
-        }}>
+        <ThemeProvider theme={theme}>
             <GlobalStyle />
             {props.children}
         </ThemeProvider>
