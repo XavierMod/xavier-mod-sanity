be79595471dfd6ec8445b13c86cb63c050f343a7
Xavier Mod <hi.xavier.mod@gmail.com>
2020-09-12 23:09:55 +0200

    possible fix - not reading theme js

--- web/src/theme/Theming.js
+++ web/src/theme/Theming.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import {ThemeProvider} from 'styled-components'
-import {theme} from './theme'
 import GlobalStyle from './globalStyles'
 
 const Theming = (props) => {
     return (
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={{
+            colors: {
+                mainBackground: '#14161A',
+                darkBackground: '#131416',
+                textPrimary: '#ffffff',
+                textDark: '#14161A',
+                accent: '#ffffff'
+            },
+            fonts: {
+                primary: 'Bitter'
+            },
+            options: {
+                maxWidthLayout: '1200px',
+                logoWidth: '28px'
+            }
+        }}>
             <GlobalStyle />
             {props.children}
         </ThemeProvider>
