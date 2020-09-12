f69d349ce238b4f03ebe94243cf96cde5dd4c0bf
Xavier Mod <hi.xavier.mod@gmail.com>
2020-09-13 00:03:51 +0200

    new attempt - adding gatsby-ssr

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -5,3 +5,17 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react"
+import Theming from './src/theme/Theming'
+import ResponsiveHeader from './src/components/Headers/ResponsiveHeader'
+import './src/styles/animations/reveal.css';
+import './src/styles/animations/basic.css';
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <Theming>
+        <ResponsiveHeader />
+        {element}
+    </Theming>
+  )
+}
